bb325d127ad4c01072468c36d0d8c77c52309e7f
Kranti Naik <kranti.newlife@gmail.com>
2025-06-11 16:32:13 +0530

    minor updates

--- blocks/custom-hero/custom-hero.js
+++ blocks/custom-hero/custom-hero.js
@@ -1,15 +1,22 @@
-export default async function decorate(block) {
 
-    const heroImgBlock = block.children[0];
-    console.log(heroImgBlock + "hi");
-  const heroImge = heroImgBlock.querySelector('picture');
-  console.log(heroImge + "hi");
+
+export default async function decorate(block) {
+  const heroImgBlock = block.children[0];
+  console.log('heroImgBlock:', heroImgBlock);
+
+  const heroImge = heroImgBlock?.querySelector('picture');
+  console.log('heroImge:', heroImge);
+
   const container = document.createElement('div');
-    container.classList.add('hero-container-test');
+  container.classList.add('hero-container-test');
+
+  if (heroImge) {
     heroImge.classList.add("hero-image");
     container.appendChild(heroImge);
+  } else {
+    console.warn('No picture element found in heroImgBlock');
+  }
 
-block.textContent = '';
-block.appendChild(container);
-
+  block.textContent = '';
+  block.appendChild(container);
 }
